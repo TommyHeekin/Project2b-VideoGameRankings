034b646e023667d331c1626a668657d73b2efa2a
Keenan Lundgren <kman5665@gmail.com>
2026-03-28 15:35:52 -0600

    Added functionality to display results

--- interface.py
+++ interface.py
@@ -5,6 +5,9 @@
 #further credit to https://github.com/avalon60/ctk_theme_builder for tutorials on color and other elements
 
 import customtkinter
+from BPlusTree import *
+from MaxHeap import *
+import random
 
 #Radiobutton, so only 1 option may be chosen
 #Class based implementations to easily scale number of options without hardcoding
@@ -54,11 +57,14 @@ class MyScrollableCheckboxFrame(customtkinter.CTkScrollableFrame):
     
 
 class App(customtkinter.CTk):
-    def __init__(self):
+    def __init__(self, myDataStructure):
         super().__init__()
 
+        #it does not matter which data structure is used 
+
+        self.dataStructure = myDataStructure
         #Create Window
-        self.title("Game Recommendation")
+        self.title("Game Recommendation App")
         self.geometry("900x780")
 
         #Ensure widgets can expand horizontally 
@@ -73,7 +79,7 @@ class App(customtkinter.CTk):
         self.scrollable_checkbox_frame.grid(row=3, column=0, padx=10, pady=(10, 0), sticky="news", columnspan=5)
 
         #search button
-        self.button = customtkinter.CTkButton(self, text="Search", command=self.button_callback, height = 40)
+        self.button = customtkinter.CTkButton(self, text="Search", command=self.button_callback, height = 40, font=("arial", 30))
         self.button.grid(row=6, column=0, padx=10, pady=10, sticky="news", columnspan=5)
         self.button.grid_columnconfigure(0, weight=1)
 
@@ -140,8 +146,9 @@ class App(customtkinter.CTk):
         self.textbox.grid(row=0, column=0, padx=10, pady=15, sticky="news", columnspan=5)
         self.textbox.grid_columnconfigure(0, weight=1)
 
-
+        #size doesnt scale automatically
         customtkinter.deactivate_automatic_dpi_awareness()
+        customtkinter.set_appearance_mode("dark")
 
     #updating rating textbox
     def updateRating(self, value):
@@ -157,10 +164,43 @@ class App(customtkinter.CTk):
     #return values for search
     def button_callback(self):
         args = [self.scrollable_checkbox_frame.get(), round(self.slider.get()/10, 1), self.scrollable_checkbox_frame1.get()]
-        print("Genres:", args[0])
-        print("Min Rating:", args[1])
-        print("Console:", args[2])
         self.slider_event(self.slider.get())
-customtkinter.set_appearance_mode("dark")
-app = App()
-app.mainloop()
+
+        self.search(args)
+
+    #use bools to determine which sorting algorithm to use
+    def search(self, values):
+        results = self.dataStructure.search(values[1])
+        ratingEnabled = True if values[1] != 0.0 else False
+        gameList = []
+        if results is not None:
+            for elem in results:
+            #searches to see if any element of a games genre is part of the users preferences
+                if any(x in elem.genre for x in values[0]):
+                    if elem.platform == values[2]:
+                        gameList.append([elem.title, elem.rating, elem.platform, elem.genre])
+        if not gameList:
+            print("No games found")
+            return
+        #always give the user a list of up to 10 games that match their specificatios, randomly shuffling the list to prevent the same games from always appearing
+        random.shuffle(gameList)
+        gameList = gameList[:10]
+        self.showGames(gameList)
+    
+
+    #new function to actually display results
+    def showGames(self, games):
+        recommendation = customtkinter.CTkToplevel(self)
+        recommendation.title("Recommended Games")
+        recommendation.geometry("600x440")
+        recommendation.attributes(topmost=True)
+        recommendation.configure(fg_color="#0e4635")
+
+        frame = customtkinter.CTkScrollableFrame(recommendation, fg_color="#1c7254")
+        frame.grid(row =0, column =0, padx=10, pady=10, sticky="nsew")
+        recommendation.grid_columnconfigure(0, weight=1)
+        recommendation.grid_rowconfigure(0, weight=1)
+        for i in range(len(games)):
+            recommended = f"Game {i+ 1}: {games[i][0]}, Rating: {games[i][1]}, Platform: {games[i][2]}, Genre: {games[i][3]}"
+            label = customtkinter.CTkLabel(frame, text=recommended, anchor ="w", wraplength=550)
+            label.pack(fill="x", padx=5, pady=5, expand=True)
